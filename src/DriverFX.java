9205ea0d9b5ad1bb10a400ccfd4b8925a55c4a70
David Dayton <54491812+BugbPoint0@users.noreply.github.com>
2020-12-02 23:21:17 -0500

    Add files via upload

---
+++ src/DriverFX.java
@@ -0,0 +1,31 @@
+
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class DriverFX extends Application {  
+	/**
+	 * The main method for the GUI JavaFX version
+	 * @param args not used
+	 * @throws IOException
+	 */
+	public static void main(String[] args) {
+		launch(args);   
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		//  instantiate the FXMainPane, name it root
+		FXMainPane root = new FXMainPane();
+		//  set the scene to hold root
+		stage.setScene(new Scene(root, 600,700));
+		//set stage title
+		stage.setTitle("Travelling Student");
+		//display the stage
+		stage.show();
+	
+	}
+
+}
---
+++ src/Town.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class Town implements Comparable<Town> {
+
+	String name;
+	ArrayList<String> neighboringTowns; 
+	
+	
+	Town(String name) {
+		
+	}
+	
+	Town(Town templateTown) {
+		
+	}
+	
+	
+	@Override
+	public int compareTo(Town o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	 public boolean equals(Object obj) {
+		return false;
+		 
+	 }
+	
+	 
+	 public String getName() {
+		return null;
+		 
+	 }
+	 
+	 
+	 public int hashCode() {
+		return 0;
+		 
+	 }
+	
+	 
+	 public String toString() {
+		return null;
+		 
+	 }
+	 
+}
